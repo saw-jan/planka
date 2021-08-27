f0a6cc5d77085d0f77b2a0fa0e617f87cc7ef92e
saw-jan <saw.jan.grg3e@gmail.com>
2021-08-27 10:48:35 +0545

    update drone config

--- client/tests/nightwatch.conf.js
+++ client/tests/nightwatch.conf.js
@@ -1,15 +1,24 @@
 const path = require('path');
 const LAUNCH_URL = process.env.LAUNCH_URL || 'http://localhost:3000';
+console.log(process.env.DRONE);
+console.log(typeof process.env.DRONE);
+const SELENIUM_HOST = process.env.DRONE === 'true' ? 'selenium' : 'localhost';
 
 module.exports = {
   page_objects_path: path.join(__dirname, 'acceptance', 'pageObjects'),
   globals_path: path.join(__dirname, 'acceptance', 'helpers', 'globals.js'),
+  screenshots: {
+    enabled: true,
+    path: './screenshots',
+    on_failure: true,
+    on_error: true,
+  },
   test_settings: {
     default: {
       launch_url: LAUNCH_URL,
       selenium: {
         start_process: false,
-        host: process.env.DRONE === true ? 'selenium' : 'localhost',
+        host: SELENIUM_HOST,
         port: 4444,
       },
       desiredCapabilities: {
